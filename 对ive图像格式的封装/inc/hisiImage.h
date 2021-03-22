73344cd55d95b1a96be3277c4343fabd310e6a1e
slave wang <1510627880@qq.com>
2021-03-22 16:52:16 +0800

    合并了两个头文件

--- 对ive图像格式的封装/inc/hisiImage.h
+++ 对ive图像格式的封装/inc/hisiImage.h
@@ -24,6 +24,37 @@
 #include "stb_image_write.h"
 #endif
 
+typedef struct{
+    int w;
+    int h;
+    int c;
+    unsigned char *data;
+} stbImageData;
+
+bool readGrayBmpImage(const char* path,stbImageData &image){
+    int x, y, channels_in_file, desired_channels = 1;
+    image.data = stbi_load(path, &x, &y, &channels_in_file, desired_channels);
+    if (!image.data)
+    {
+        fprintf(stderr, "fail to read image: %s\n", path);
+        return false;
+    }
+    fprintf(stdout, "image: %s, x: %d, y: %d, channels_in_file: %d, desired_channels: %d\n",
+            path, x, y, channels_in_file, desired_channels);
+    image.w = x;
+    image.h = y;
+    image.c = 1;
+    printf("%s end\n", __FUNCTION__);
+    return true;
+}
+
+bool writeGrayBmpImage(const char* path,stbImageData &image){
+    int ret = stbi_write_bmp(path, image.w, image.h, image.c, image.data);
+}
+
+
+
+
 #define IVE_MMZ_FREE(phy, vir)                                  \
     do                                                          \
     {                                                           \
