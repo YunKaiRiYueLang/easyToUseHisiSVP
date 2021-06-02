edf4b8f3d3a5a591425b27ba9ac7baa27bde6e1f
1 <1>
2021-06-02 16:09:08 +0800

    增加了image io的头文件

---
+++ 对ive图像格式的封装/涉及opencv/imageio.h
@@ -0,0 +1,25 @@
+#ifndef IMAGE_IO_H
+#define IMAGE_IO_H
+
+#include"hi_ive.h"
+
+class hisiImage;//类声明。
+#include"commonHeader.h"
+
+typedef struct
+{
+    int w;
+    int h;
+    int c;
+    unsigned char *data;
+} stbImageData;
+
+bool readGrayBmpImage(const char *path, stbImageData &image);
+bool writeGrayBmpImage(const char *path, stbImageData &image);
+bool writeColorBmpImage(const char *path, const hisiImage &src);
+bool writeGrayBmpImage(const char *path, const hisiImage &src);
+bool writeiveImage(const char *path, IVE_IMAGE_S src);
+bool writeIveImage_jpg(const char *path, IVE_IMAGE_S src);
+bool iveImageToMat(Mat &outCvImage, IVE_IMAGE_S &inputIveImage);
+bool MatU8C1ImageToIve(const Mat &input, IVE_IMAGE_S &output);
+#endif
