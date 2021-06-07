1c015e7b214a5fc35f2510aced9ff492cc3a1ffe
1 <1>
2021-06-07 10:45:40 +0800

    删除和和合并两个文件

--- 对ive图像格式的封装_涉及opencv/inc/hisiImage.h
+++ 对ive图像格式的封装_涉及opencv/inc/hisiImage.h
@@ -214,4 +214,24 @@ public:
     // HI_U32 au32Stride[3];  /* RW;The stride of the image */
 };
 
+
+
+typedef struct
+{
+    int w;
+    int h;
+    int c;
+    unsigned char *data;
+} stbImageData;
+
+bool readGrayBmpImage(const char *path, stbImageData &image);
+bool writeGrayBmpImage(const char *path, stbImageData &image);
+bool writeColorBmpImage(const char *path, const hisiImage &src);
+bool writeGrayBmpImage(const char *path, const hisiImage &src);
+bool writeiveImage(const char *path, IVE_IMAGE_S src);
+bool writeIveImage_jpg(const char *path, IVE_IMAGE_S src);
+#ifdef OPENCV_CORE_MAT_HPP
+bool iveImageToMat(Mat &outCvImage, IVE_IMAGE_S &inputIveImage);
+bool MatU8C1ImageToIve(const Mat &input, IVE_IMAGE_S &output);
+#endif
 #endif
