f61dd51d88b4b2443bea5b964b45e8e4abd556e4
gzkm <1510627880@qq.com>
2023-02-21 11:01:09 +0800

    增加3403的相关函数

--- 3403_code/inc/iveFunctions.hpp
+++ 3403_code/inc/iveFunctions.hpp
@@ -1,3 +1,8 @@
 #ifdef hi_3403
 bool iveCsc(ot_svp_img &src, ot_svp_img &dst, const int mode, const int needBlock)
+// transform hisi yuv (BT 709 :420 sp / 422 sp ) to rgb (0~255) package;
+// use guarantees the security of the two parameters
+bool yuv2rgbIve(ot_video_frame_info *frame_info, void *rgb);
+bool iveCsc(hisiImage &src, hisiImage &dst, const int mode, const int needBlock);
+void MediaPatrol(int roiWidth, int roiHeight, uint8_t *yuvBuffer, uint8_t **dstEdgeBuffer, int high_th = 150, int low_th = 50);
 #endif
--- 3403_code/src/iveFunctions.cpp
+++ 3403_code/src/iveFunctions.cpp
@@ -86,4 +86,143 @@ bool iveCsc(ot_svp_img &src, ot_svp_img &dst, const int mode, const int needBloc
     CHECK_IVE_FUNCTION("ss_mpi_ive_csc", s32Result);
     BLOCK_IVE_FUNCTION(needBlock, handle);
 }
+// transform hisi yuv (BT 709 :420 sp / 422 sp ) to rgb (0~255) package;
+// vir_addr  phy_addr  w h s 
+bool yuv2rgbIve(ot_video_frame_info *frame_info, void *rgb)
+{
+    
+    hisiImage hisiSrc(*frame_info);
+    hisiImage hisiDst;
+    bool ret = false;
+    hisiDst.create(frame_info->video_frame.width, frame_info->video_frame.height, OT_SVP_IMG_TYPE_U8C3_PACKAGE);
+    if (hisiSrc.iveImg.type == OT_SVP_IMG_TYPE_YUV420SP || hisiSrc.iveImg.type == OT_SVP_IMG_TYPE_YUV422SP)
+    {
+        ret = iveCsc(hisiSrc, hisiDst, 3, 1);
+    }
+    else
+    {
+        errorCode("do not support the pixel format", 0);
+    }
+    if (ret)
+    {
+        memcpy(rgb,(void*)hisiDst.iveImg.virt_addr[0],hisiDst.iveImg.stride[0]*hisiDst.iveImg.height*3);
+    }
+    return true;
+}
+/**
+ * @brief
+ *
+ * @param src
+ * @param dst
+ * @param mask kernel value
+ * @param norm normalization value
+ * @param isInstant  designate 0 or 1
+ * @return true
+ * @return false
+ */
+bool iveFilter(hisiImage &src, hisiImage &dst, signed char mask[25], unsigned char norm, int isInstant)
+{
+    ot_svp_img iveSrc = src.getIVEImage();
+    ot_svp_img iveDst = dst.getIVEImage();
+    if (iveSrc.type != OT_SVP_IMG_TYPE_U8C1 && iveSrc.type != OT_SVP_IMG_TYPE_YUV420SP && iveSrc.type != OT_SVP_IMG_TYPE_YUV422SP)
+    {
+        errorCode("\nsrc image type error,type must be u8c1,yuv420sp,yuv422sp\n", -1);
+    }
+    if (iveSrc.type != iveDst.type)
+    {
+        errorCode("\nerror: src iamge type != dst image type\n", -2);
+    }
+    CHECK_SIZE(iveSrc, 64, 64, 1920, 1024);
+    CHECK_SIZE(iveDst, 64, 64, 1920, 1024);
+    if (iveSrc.width != iveDst.width || iveSrc.height != iveDst.height)
+    {
+        errorCode("\nsrc dst must have the same image size\n", -3);
+    }
+    int handle;
+    ot_ive_filter_ctrl ctrl;
+    ctrl.norm = norm;
+    memcpy(ctrl.mask, mask, 25);
+    int retCode = 0;
+    retCode = ss_mpi_ive_filter(&handle, &iveSrc, &iveDst, &ctrl, static_cast<td_bool>(isInstant));
+    CHECK_IVE_FUNCTION_RET("ss_mpi_ive_filter", retCode, false);
+    BLOCK_IVE_FUNCTION(isInstant, handle);
+    return true;
+}
+
+bool iveCanny(hisiImage &src, hisiImage &dst, signed char mask[25], int high_th = 150, int low_th = 50, int isInstant = 0)
+{
+    ot_svp_img svpSrc = src.getIVEImage();
+    ot_svp_img svpDst = dst.getIVEImage();
+    CHECK_SIZE(svpSrc, 64, 64, 1920, 1024);
+    CHECK_SIZE(svpDst, 64, 64, 1920, 1024);
+    if (svpSrc.type != OT_SVP_IMG_TYPE_U8C1)
+    {
+        errorCode("\nsrc image type error,type must be u8c1\n", -1);
+    }
+    if (svpSrc.type != svpDst.type)
+    {
+        errorCode("\nerror: src iamge type != dst image type\n", -2);
+    }
+    ot_svp_mem_info stack;
+    stack.size = svpSrc.width * svpSrc.height * sizeof(ot_svp_point_u16) + sizeof(ot_ive_canny_stack_size);
+    stack.size = HI_CalcStride(stack.size, 16); //
+    int ret = ss_mpi_sys_mmz_alloc(&(stack.phys_addr), (void **)&(stack.virt_addr),
+                                   TD_NULL, TD_NULL, stack.size);
+    if (ret != 0)
+    {
+        errorCode("ss_mpi_sys_mmz_alloc", ret);
+        return false;
+    }
+    ot_ive_canny_hys_edge_ctrl ctrl;
+    ctrl.mem.size = HI_CalcStride(svpSrc.width * svpSrc.height * 3, 16);
+    ret = ss_mpi_sys_mmz_alloc(&(ctrl.mem.phys_addr), (void **)&(ctrl.mem.virt_addr),
+                               TD_NULL, TD_NULL, ctrl.mem.size);
+    if (ret != 0)
+    {
+        errorCode("ss_mpi_sys_mmz_alloc", ret);
+        return false;
+    }
+    int handle;
+    ctrl.high_threshold = high_th;
+    ctrl.low_threshold = low_th;
+    memcpy(ctrl.mask, mask, 25);
+    ret = ss_mpi_ive_canny_hys_edge(&handle, &svpSrc, &svpDst, &stack, &ctrl, static_cast<td_bool>(isInstant));
+    BLOCK_IVE_FUNCTION(isInstant, handle);
+    CHECK_IVE_FUNCTION_RET("ss_mpi_ive_canny_hys_edge", ret, false);
+
+    ret = ss_mpi_ive_canny_edge(&svpDst, &stack);
+    CHECK_IVE_FUNCTION_RET("ss_mpi_ive_canny_edge", ret, false);
+    ss_mpi_sys_mmz_free((td_phys_addr_t)(ctrl.mem.phys_addr), (td_void *)(td_uintptr_t)(ctrl.mem.virt_addr));
+    ss_mpi_sys_mmz_free((td_phys_addr_t)(stack.phys_addr), (td_void *)(td_uintptr_t)(stack.virt_addr));
+}
+
+// note:
+// #define STB_IMAGE_WRITE_IMPLEMENTATION
+// #include "stb_image_write.h"
+void MediaPatrol(int roiWidth, int roiHeight, uint8_t *yuvBuffer, uint8_t **dstEdgeBuffer, int high_th = 150, int low_th = 50)
+{
+    hisiImage src;
+    src.create(roiWidth, roiHeight, OT_SVP_IMG_TYPE_U8C1);
+    hisiImage dst;
+    dst.create(roiWidth, roiHeight, OT_SVP_IMG_TYPE_U8C1);
+    signed char ps8Mask[25] = {0, 0, 0, 0, 0,
+                               0, -1, 0, 1, 0,
+                               0, -2, 0, 2, 0,
+                               0, -1, 0, 1, 0,
+                               0, 0, 0, 0, 0};
+    signed char mask[25] = {0, 0, 0, 0, 0,
+                            0, 1, 2, 1, 0,
+                            0, 2, 4, 2, 0,
+                            0, 1, 2, 1, 0,
+                            0, 0, 0, 0, 0};
+    int size = dst.iveImg.height * dst.iveImg.width;//64*64
+    memcpy((void*)src.iveImg.virt_addr[0],yuvBuffer,size);                       
+    iveFilter(src, dst, mask, 4, 0);
+    iveCanny(src, dst, ps8Mask, high_th, low_th, 1);
+    *dstEdgeBuffer = (uint8_t *)malloc(size);
+    memcpy(*dstEdgeBuffer, (void *)(dst.iveImg.virt_addr[0]), size);
+    // stbi_write_bmp("/mnt/wbj/src.bmp",src.iveImg.stride[0],src.iveImg.height,1,(void*)src.iveImg.virt_addr[0]);
+    // stbi_write_bmp("/mnt/wbj/edge.bmp",dst.iveImg.stride[0],dst.iveImg.height,1,*dstEdgeBuffer);
+}
+
 #endif
